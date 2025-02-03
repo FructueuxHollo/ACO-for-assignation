ff41bfdf7d00c20d8acd6b7081d4bafb017b8960
FructueuxHollo <fructueuxhollo@gmail.com>
2025-02-03 22:55:15 +0100

    feat: random search for fine tuning

--- utils/constants.py
+++
@@ -1,5 +0,0 @@
-verbose = True
-find_optimal = True
-all_jobs = False
-fine_tune = False
-jobs = 9
---
+++ utils/options.py
@@ -0,0 +1,6 @@
+verbose = False
+find_optimal = False
+all_jobs = False
+fine_tune = True
+jobs = 36
+animate = False
